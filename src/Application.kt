e614ed03cfcc2e23b5ecf2647adc255d0c90495a
caleb.xiang <caleb.xiang@gmail.com>
2020-07-24 20:29:26 +0800

    check back

--- src/Application.kt
+++ src/Application.kt
@@ -39,17 +39,17 @@ fun Application.module(testing: Boolean = false) {
     val client = HttpClient(Apache) {
     }
 
-//    val translate = TranslateOptions
-//        .newBuilder()
-//        .setCredentials(
-//            ServiceAccountCredentials
-//                .fromStream(
-//                    FileInputStream(
-//                        "/usr/local/etc/xiang-pi.json"
-//                    )
-//                )
-//        )
-//        .build().service
+    val translate = TranslateOptions
+        .newBuilder()
+        .setCredentials(
+            ServiceAccountCredentials
+                .fromStream(
+                    FileInputStream(
+                        "/Users/cxiang/code/xiang-pi-61750bc51e20.json"
+                    )
+                )
+        )
+        .build().service
 
     routing {
         get("/") {
@@ -60,41 +60,41 @@ fun Application.module(testing: Boolean = false) {
             call.respond(data)
         }
 
-//        get("/t/{text}") {
-//            val text = call.parameters.get("text")
-//            val translatedText = translate.translate(
-//                text,
-//                Translate.TranslateOption.sourceLanguage("en"),
-//                Translate.TranslateOption.targetLanguage("zh-CN")
-//            );
-//            val data = ResponseData(
-//                true,
-//                translatedText.translatedText
-//            )
-//            call.respond(data)
-//        }
+        get("/t/{text}") {
+            val text = call.parameters.get("text")
+            val translatedText = translate.translate(
+                text,
+                Translate.TranslateOption.sourceLanguage("en"),
+                Translate.TranslateOption.targetLanguage("zh-CN")
+            );
+            val data = ResponseData(
+                true,
+                translatedText.translatedText
+            )
+            call.respond(data)
+        }
 
-//        get("/q/{text}") {
-//            val lines = readPoems().map { it -> it.paragraphs }
-//                .flatten()
-//            val suggester = Suggester()
-//            for (line in lines) {
-//                suggester.addSentence(line)
-//            }
-//            val text = call.parameters.get("text")
-//            val translatedText = translate.translate(
-//                text,
-//                Translate.TranslateOption.sourceLanguage("en"),
-//                Translate.TranslateOption.targetLanguage("zh-CN")
-//            );
-//            val result = suggester.suggest(translatedText.translatedText, 1)
-//            val data = ResponseData(
-//                true,
-//                result[0]
-//            )
-//
-//            call.respond(data)
-//        }
+        get("/q/{text}") {
+            val lines = readPoems().map { it -> it.paragraphs }
+                .flatten()
+            val suggester = Suggester()
+            for (line in lines) {
+                suggester.addSentence(line)
+            }
+            val text = call.parameters.get("text")
+            val translatedText = translate.translate(
+                text,
+                Translate.TranslateOption.sourceLanguage("en"),
+                Translate.TranslateOption.targetLanguage("zh-CN")
+            );
+            val result = suggester.suggest(translatedText.translatedText, 1)
+            val data = ResponseData(
+                true,
+                result[0]
+            )
+
+            call.respond(data)
+        }
     }
 }
 
